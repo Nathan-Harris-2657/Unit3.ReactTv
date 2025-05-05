3e62f3cda60293b47584fa54c73bcc58550995be
Nathan Harris <singdubharris@gmail.com>
2025-05-04 21:18:04 -0400

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,33 @@
  * React TV is an web streaming platform that allows users to browse
  * through the episodes of a variety of different shows.
  */
+
+
+import { useState } from "react";
+import EpisodeDetails from "./episodes/EpisodeDetails";
+import EpisodeList from "./episodes/EpisodeList";
+import ShowSelection from "./shows/ShowSelection";
+import { tvShows } from "./shows/data";
+
 export default function App() {
+  const [selectedShow, setSelectedShow] = useState(tvShows[0]); // Default to first show
+  const [selectedEpisode, setSelectedEpisode] = useState(null); // Default to no episode
+
   return (
     <>
       <header>
         <p>React TV</p>
       </header>
-      <main></main>
+      <main>
+        <ShowSelection tvShows={tvShows} setSelectedShow={setSelectedShow} />
+        <EpisodeList 
+          name={selectedShow.name} 
+          episodes={selectedShow.episodes} 
+          selectedEpisode={selectedEpisode} 
+          setSelectedEpisode={setSelectedEpisode} 
+        />
+        <EpisodeDetails episode={selectedEpisode} />
+      </main>
     </>
   );
 }
--- src/shows/ShowSelection.jsx
+++ src/shows/ShowSelection.jsx
@@ -1,6 +1,19 @@
+import { tvShows } from "./data";
 import "./shows.css";
 
 /** A navbar that allows users to choose between a list of shows */
-export default function ShowSelection() {
-  return <nav className="shows"></nav>;
+export default function ShowSelection({ tvShows, setSelectedShow }) {
+  return (
+    <nav className="shows">
+      {tvShows.map((show) => (
+        <div 
+          key={show.id} 
+          onClick={() => setSelectedShow(show)} // Update selected show
+          style={{ cursor: "pointer", padding: "5px" }}
+        >
+          {show.name}
+        </div>
+      ))}
+    </nav>
+  );
 }
